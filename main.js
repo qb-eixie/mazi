93c94ebf18b30637eb4a7506726055a4b3667dd1
unknown <kalyan.raparthi@hotmail.com>
2023-10-18 14:07:42 +0530

    + getter(), postter()

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
 const express = require("express");
 var bodyParser = require('body-parser')
-const fs = require("fs");
-const { stringify } = require("querystring");
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+a = []; a.push({ pp: false, p: false, st: true, s: 0 })
+
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html")
+    console.log("get/")
 })
-var a = [];
+
+app.get('/data', (req, res) => {
+    console.log("get/data");
+      res.json(a[0]);
+})
+
 app.post('/data', (req, res) => {
-    a.pop()
-    a.push(req.body)
+        console.log("get/data")
+        a.pop()
+        a.push(req.body)
 })
 
-function aa() {console.log(a[0])}
-
-setInterval(aa, 1000)
+setInterval( function(){ console.log(a[0])}, 4000);
 
 app.listen(3000, () => {
     console.log("...")
 })
-
